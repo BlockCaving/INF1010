95e68837951f75a3a9b769da8affb7e7d9ac5970
Claude Gagné <claudegag@sansfil-securise-etudiants-lassonde-240-60.polymtl.ca>
2017-09-20 09:11:18 -0400

    osx

--- Const.h
+++ Const.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Constante de couleur pour les pixels.
+// Constante de couleur pour les pixels
 const int MIN_CONCENTRATION_COULEUR = 0;
 const int MAX_CONCENTRATION_COULEUR = 255;
 
